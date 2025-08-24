8293b7febee967c171931eaaeaca06ae4af4e107
yuichi.nasukawa <nasu0227@gmail.com>
2025-08-24 13:37:30 +0000

    feat: enhance Django admin interface with custom configuration

--- polls/admin.py
+++ polls/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
@@ -11,8 +11,7 @@ class ChoiceInline(admin.StackedInline):
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
-        ("Date information", {"fields": [
-         "pub_date"], "classes": ["collapse"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]})
     ]
     inlines = [ChoiceInline]
     list_display = ["question_text", "pub_date", "was_published_recently"]
